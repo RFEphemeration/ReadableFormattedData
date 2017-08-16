d7c1ab8872873343a1c627de2cbe3fcda92abfdd
Robinson Farrar <robinson.farrar@gmail.com>
2017-08-15 20:31:47 -0400

    most basic include
    relative paths aren't implemented
    no checks for cycles
    no saved copies to avoid doing duplicate work

--- src/RFDMacros.py
+++ src/RFDMacros.py
@@ -6,9 +6,15 @@ def RemoveStringDelimeter(context, arguments):
 	for delimeter in arguments:
 		context.potential_string_delimeters.remove(delimeter)
 
+def IncludeAll(context, arguments):
+	f = open (arguments[0], 'r')
+	remainder = f.read()
+	context.remainder = remainder + '\n' + context.remainder
+
 MacroFunctions = {
 	'add_string_delimeter' : AddStringDelimeter,
-	'remove_string_delimeter' : RemoveStringDelimeter
+	'remove_string_delimeter' : RemoveStringDelimeter,
+	'include_all': IncludeAll,
 }
 
 def ExecuteMacro(context, command, arguments):
